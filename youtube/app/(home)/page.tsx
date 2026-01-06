0370de6fadd30139649fc1f530b2cdd35679c9dd
MOGAT0 <mogatosamjefferson@gmail.com>
2026-01-06 12:01:24 +0800

    feature: trpc setup finished

--- youtube/app/(home)/page.tsx
+++ youtube/app/(home)/page.tsx
@@ -1,11 +1,22 @@
-import { Button } from "@/components/ui/button"
-import Image from 'next/image'
+import { HydrateClient, trpc } from "@/trpc/server";
+import { PageClient } from "./client";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
-export default function Home() {
+export default async function Home() {
+
+  void trpc.hello.prefetch({ text:"world"});
+
   return (
     <div>
       {/* <Image src={"/icon.svg"} width={50} height={50} alt="Logo"/> */}
-      <p className="text-xl font-semibold tracking-tight">Videos will be displayed here</p>
+      <HydrateClient>
+        <Suspense fallback = {<p>loading...</p>}>
+        <ErrorBoundary fallback = {<p>Error...</p>}>
+          <PageClient/>
+        </ErrorBoundary>
+        </Suspense>
+      </HydrateClient>
     </div>
   )
 }
